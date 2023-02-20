3b0bad8f35291fc0d0ca563732a8c16a4f58c376
leminhthai0303 <leminhthai0303@gmail.com>
2023-02-20 08:11:04 +0700

    Finish ver

--- scripts.js
+++ scripts.js
@@ -1,52 +1,82 @@
-const type = ["rock", "paper", "scissors"];
+const type = ['rock', 'paper', 'scissors'];
 
-function playerSelection(){
-    let ans = prompt("Choose: Rock, Paper, or Scissors")
-    return ans
+const buttons = document.querySelectorAll('button')
+const cText = document.getElementById('cText');
+
+const h2 = document.createElement('h2');
+h2.textContent = 'Welcome to the game!';
+cText.appendChild(h2);
+
+const playerText = document.createElement('h3');
+const compText = document.createElement('h3');
+const resultText = document.createElement('h3');
+resultText.textContent = '';
+cText.append(playerText, compText, resultText);
+
+function compChoice() {
+    const comp = type[Math.floor(Math.random()*type.length)];
+    return comp
 }
 
-function compChoice(){
-    return type[Math.floor(Math.random()*type.length)];
-}
 
-function score(player, comp){
+function play(player, comp) {
     if (player === comp) {
-        return "tie"
-    } else 
-    if ((player === "rock" && comp === "scissors") || (player === "paper" && comp === "rock") || (player === "scissors" && comp === "paper")) {
-        return "win"
-    }
-    else {
-        return "lose"
-    }
+        return 'T';
+      } else if (
+        (player === 'rock' && comp === 'scissors') ||
+        (player === 'paper' && comp === 'rock') ||
+        (player === 'scissors' && comp === 'paper')
+      ) {
+        return 'W';
+      } else {
+        return 'L';
+      }
 }
 
-var playerPoints = 0;
-var compPoints = 0;
+function main() {
+    let playerPoints = 0
+    let compPoints = 0
+    let round = 0
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            if (round >= 5) {
+                alert('Game over!');
+                return;
+            }
+            let player = button.id
+            let comp = compChoice()
+            let result = play(player, comp)
+            if (result === "W") {
+                playerPoints += 1
+                playerText.textContent = `YOU chose: ${player} - You points: ${playerPoints}`
+                compText.textContent = `BOT chose: ${comp} - Computer points: ${compPoints}`
+                round += 1
+            } else
+            if (result === "L") {
+                compPoints += 1
+                playerText.textContent = `YOU chose: ${player} - You points: ${playerPoints}`
+                compText.textContent = `BOT chose: ${comp} - Computer points: ${compPoints}`
+                round += 1
+            } else
+            if (result === "T") {
+                playerText.textContent = `YOU chose: ${player} - You points: ${playerPoints}`
+                compText.textContent = `BOT chose: ${comp} - Computer points: ${compPoints}`
+                round += 1
+            }
+            if (round >= 5) {
+                setTimeout(() => {
+                  if (playerPoints > compPoints) {
+                    alert('You win!');
+                  } else if (compPoints > playerPoints) {
+                    alert('You lose!');
+                  } else {
+                    alert('Tied!');
+                  }
+                }, 1); // Delay alert by 1 second to show player and computer choices
+              }
+        })
+    })
 
-for (let i=0;i<5;i++) {
-    var player = playerSelection()
-    var comp = compChoice()
-    console.log(player)
-    console.log(comp)
-    let res = score(player, comp)
-    if (res === "win"){
-        playerPoints += 1
-    }
-    else if (res === "lose") {
-        compPoints += 1
-    }
-    else;
-    console.log('Your points:', playerPoints)
-    console.log('Comp points:', compPoints)
 }
 
-if (playerPoints < compPoints) {
-    console.log("You LOSE!!!")
-}
-else if (playerPoints == compPoints) {
-    console.log("It's a TIE")
-}
-else {
-    console.log("You WIN!!! Congrats")
-}
+main();
